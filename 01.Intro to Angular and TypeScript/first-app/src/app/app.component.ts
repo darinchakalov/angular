14cdd3a81f0651623c16fe2a9e254a17e5706cad
darinchakalov <darinchakalov@hotmail.com>
2022-02-06 01:59:25 +0200

    binding and interpolation

--- 01.Intro to Angular and TypeScript/first-app/src/app/app.component.ts
+++ 01.Intro to Angular and TypeScript/first-app/src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'First App';
+
+  username = 3;
+
+  users = [
+    {
+      name: 'Ivan 1',
+      age: 21,
+    },
+    {
+      name: 'Ivan 2',
+      age: 22,
+    },
+    {
+      name: 'Ivan 3',
+      age: 23,
+    },
+  ];
+
+  buttonClickHandler(): void {
+    let current = this.username++;
+    this.users.push({
+      name: 'Ivan ' + current,
+      age: 20 + current,
+    });
+  }
 }
