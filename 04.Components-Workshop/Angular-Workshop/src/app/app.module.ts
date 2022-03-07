8db5a05a82f51e8545fb10f42ba7caa8ef9d1b0f
darinchakalov <darinchakalov@hotmail.com>
2022-03-07 04:13:02 +0200

    created content service, post and theme interfaces

--- 04.Components-Workshop/Angular-Workshop/src/app/app.module.ts
+++ 04.Components-Workshop/Angular-Workshop/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ThemeComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
--- 04.Components-Workshop/Angular-Workshop/src/app/core/core.module.ts
+++ 04.Components-Workshop/Angular-Workshop/src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, AsideComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   //Creating this exports so they can be used in the app module
   exports: [HeaderComponent, FooterComponent, AsideComponent],
 })
