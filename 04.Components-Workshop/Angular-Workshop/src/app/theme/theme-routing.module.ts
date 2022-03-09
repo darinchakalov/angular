4107a4ace793d0e143c3b58012b03ffabc78f3d3
darinchakalov <darinchakalov@hotmail.com>
2022-03-09 18:54:18 +0200

    created single theme component

--- 04.Components-Workshop/Angular-Workshop/src/app/theme/theme-routing.module.ts
+++ 04.Components-Workshop/Angular-Workshop/src/app/theme/theme-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewThemeComponent } from './new-theme/new-theme.component';
+import { ThemeComponent } from './theme/theme.component';
 import { ThemesComponent } from './themes/theme.component';
 
 const routes: Routes = [
   {
     path: 'themes',
-    component: ThemesComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ThemesComponent,
+      },
+      {
+        path: ':themeId',
+        component: ThemeComponent,
+      },
+    ],
   },
   {
     path: 'new-theme',
--- 04.Components-Workshop/Angular-Workshop/src/app/theme/theme.module.ts
+++ 04.Components-Workshop/Angular-Workshop/src/app/theme/theme.module.ts
@@ -4,9 +4,10 @@ import { ThemesComponent } from './themes/theme.component';
 import { NewThemeComponent } from './new-theme/new-theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
 import { AsideComponent } from './aside/aside.component';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
-  declarations: [ThemesComponent, NewThemeComponent, AsideComponent],
+  declarations: [ThemesComponent, NewThemeComponent, AsideComponent, ThemeComponent],
   imports: [CommonModule, ThemeRoutingModule],
 })
 export class ThemesModule {}
