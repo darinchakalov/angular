15c882745546df6a2989eccb6291cbd9600107ad
darinchakalov <darinchakalov@hotmail.com>
2022-03-17 07:03:55 +0200

    register form with validation directive

--- 03.Services/app/src/app/forms/forms.module.ts
+++ 03.Services/app/src/app/forms/forms.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
+import { SameValueDirective } from './same-value.directive';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, SameValueDirective],
   imports: [CommonModule, FormsModule],
 })
 export class MyFormsModule {}
--- 03.Services/app/src/app/forms/register/register.component.ts
+++ 03.Services/app/src/app/forms/register/register.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  @ViewChild(NgForm) form: NgForm | undefined 
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  registerHandler(form: NgForm) {
+    console.log(form);
   }
-
 }
