48ea6b57c1ab27fd77460bdbb112f7470318d83e
darinchakalov <darinchakalov@hotmail.com>
2022-03-17 16:16:07 +0200

    lab form

---
+++ 05.Directives and forms/app/src/app/form/form.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+})
+export class FormComponent implements AfterViewInit {
+  @ViewChild('form') form!: NgForm;
+
+  codes!: string[];
+  ngCodes!: string;
+  buildings!: string[];
+  ngBuldings!: string;
+
+  ngAfterViewInit(): void {
+    this.codes = ['+359', '+555'];
+    this.ngCodes = this.codes[0];
+    this.buildings = ['Designer', 'Coder'];
+    this.ngBuldings = this.buildings[0];
+  }
+
+  submitHandler() {
+    console.log(this.form.value);
+  }
+}
